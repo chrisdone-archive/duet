91df779e7a08149f01765ec67b054b508a9576a0
Chris Done <chrisdone@gmail.com>
2017-04-22 13:17:39 +0100

    Descriptive

--- src/Duet/Parser.hs
+++ src/Duet/Parser.hs
@@ -15,8 +15,8 @@ parseText :: SourceName -> Text -> Either ParseError (Expression Location)
 parseText fp inp =
   case parse tokensTokenizer fp inp of
     Left e -> Left e
-    Right tokens ->
-      case parse tokensParser fp tokens of
+    Right tokens' ->
+      case parse tokensParser fp tokens' of
         Left e -> Left e
         Right ast -> Right ast
 
@@ -28,29 +28,33 @@ expParser = do
   ifParser <|> varParser
 
 varParser :: Stream s m (Token, Location) => ParsecT s u m (Expression Location)
-varParser = do
-  (v, loc) <-
-    consumeToken
-      (\case
-         Variable i -> Just i)
-  pure (VariableExpression loc (Identifier (T.unpack v)))
+varParser = go <?> "variable (e.g. ‘foo’, ‘id’, etc.)"
+  where
+    go = do
+      (v, loc) <-
+        consumeToken
+          (\case
+             Variable i -> Just i
+             _ -> Nothing)
+      pure (VariableExpression loc (Identifier (T.unpack v)))
 
 ifParser :: Stream s m (Token, Location) => ParsecT s u m (Expression Location)
-ifParser = do
-  (_, loc) <- satisfyToken (== If)
-  p <- expParser
-  _ <- satisfyToken (== Then)
-  e1 <- expParser
-  _ <- satisfyToken (== Else)
-  e2 <- expParser
-  pure
-    (IfExpression
-       loc
-       { locationEndLine = locationEndLine (expressionLocation loc e2)
-       , locationEndColumn = locationEndColumn (expressionLocation loc e2)
-       }
-       p
-       e1
-       e2)
+ifParser = go <?> "if expression (e.g. ‘if p then x else y’)"
   where
+    go = do
+      (_, loc) <- satisfyToken (== If)
+      p <- expParser
+      _ <- satisfyToken (== Then)
+      e1 <- expParser
+      _ <- satisfyToken (== Else)
+      e2 <- expParser
+      pure
+        (IfExpression
+           loc
+           { locationEndLine = locationEndLine (expressionLocation loc e2)
+           , locationEndColumn = locationEndColumn (expressionLocation loc e2)
+           }
+           p
+           e1
+           e2)
     expressionLocation nil e = foldr const nil e
