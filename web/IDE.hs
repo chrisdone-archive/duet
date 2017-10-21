21a690964948d78b83c96974a92a4aa236ce048c
Chris Done <chrisdone@gmail.com>
2017-10-21 15:54:45 +0100

    Basic variable typing

--- web/IDE.hs
+++ web/IDE.hs
@@ -2,11 +2,13 @@
 
 module Main where
 
+import           Control.Arrow
 import           Control.Concurrent
 import           Control.DeepSeq
 import           Control.Monad.IO.Class
 import           Control.Monad.State (execStateT, StateT, get, put, modify)
 import           Data.Aeson
+import           Data.Monoid
 import           Data.Text (Text)
 import qualified Data.Text as T
 import           Duet.Types
@@ -115,6 +117,14 @@ interpretAction =
             Nothing -> do
               e <- liftIO (newVariableExpression [c])
               interpretAction (PutExpression e)
+            Just cursor ->
+              put
+                s
+                { stateAST =
+                    fmap
+                      (transformNode (cursorNode cursor) (insertCharVariable c))
+                      (stateAST s)
+                }
 
 interpretKeyPress :: Int -> StateT State IO ()
 interpretKeyPress k = do
@@ -124,6 +134,38 @@ interpretKeyPress k = do
       case codeAsLetter k of
         Nothing -> pure ()
         Just c -> interpretAction (InsertChar c)
+
+--------------------------------------------------------------------------------
+-- Interpreter utilities
+
+insertCharVariable :: Char
+                   -> Expression Ignore Identifier Label
+                   -> Expression Ignore Identifier Label
+insertCharVariable char =
+  \case
+    VariableExpression l (Identifier s) ->
+      VariableExpression l (Identifier (s ++ [char]))
+    e -> e
+
+transformNode
+  :: UUID
+  -> (Expression Ignore Identifier Label -> Expression Ignore Identifier Label)
+  -> Expression Ignore Identifier Label
+  -> Expression Ignore Identifier Label
+transformNode uuid f e =
+  if labelUUID (expressionLabel e) == uuid
+    then f e
+    else case e of
+           ApplicationExpression l e1 e2 ->
+             ApplicationExpression l (go e1) (go e2)
+           LambdaExpression l (Alternative al ps e) ->
+             LambdaExpression l (Alternative al ps (go e))
+           IfExpression l a b c -> IfExpression l (go a) (go b) (go c)
+           CaseExpression l e alts ->
+             CaseExpression l (go e) (map (second go) alts)
+           _ -> e
+  where
+    go = transformNode uuid f
 
 codeAsLetter :: Int -> Maybe Char
 codeAsLetter i =
@@ -146,4 +188,25 @@ newVariableExpression name = do
 
 -- | The app's view.
 appview :: State -> () -> ReactElementM ViewEventHandler ()
-appview _ _ = pure ()
+appview state _ =
+  case stateAST state of
+    Nothing -> pure ()
+    Just ast -> renderAST (stateCursor state) ast
+
+renderAST
+  :: Maybe Cursor
+  -> Expression Ignore Identifier Label
+  -> ReactElementM ViewEventHandler ()
+renderAST mcursor =
+  \case
+    VariableExpression label (Identifier ident) ->
+      Flux.div_
+        [ "key" @= labelUUID label
+        , "className" @=
+          ("duet-variable " <>
+           (if Just (labelUUID label) == fmap cursorNode mcursor
+              then "duet-selected" :: Text
+              else "duet-unselected"))
+        ]
+        (Flux.elemText (T.pack ident))
+    _ -> pure ()
