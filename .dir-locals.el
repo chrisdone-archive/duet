cad008d032117017bc2dfc368d78faff0e1c5884
Chris Done <chrisdone@gmail.com>
2019-11-19 13:44:42 +0000

    Rm .dirlocals

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((haskell-mode
-  (intero-targets "duet:lib" "duet:exe:duet" "duet:test:duet-test")))
